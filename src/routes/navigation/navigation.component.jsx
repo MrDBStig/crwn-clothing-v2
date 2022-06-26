d0b82c96b235b9637afcd2a0f75a6f6eb838f0bb
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-06-26 19:48:15 +0000

    Create sign out functionality and conditional link rendering

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,24 +1,40 @@
-import { Fragment } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { useUserContext } from "../../contexts/user.context";
 
-import './navigation.styles.scss';
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const { currentUser, setCurrentUser } = useUserContext();
+
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
+  };
+
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <CrwnLogo className='logo' />
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className='nav-link' to='/auth'>
-            SIGN IN
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutHandler}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
