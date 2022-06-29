f5c74dbe6dd34170e0800a972243e7a8422819a5
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-06-29 14:57:46 +0000

    Create remove from cart and clear item functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -15,21 +15,53 @@ const addToCart = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, productToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === productToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+const clearCartItem = (cartItems, productToClear) => {
+  return cartItems.filter((cartItem) => cartItem.id !== productToClear.id);
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
+  total: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addToCart(cartItems, productToAdd));
+  };
+
+  const removeItemFromCart = (productToRemove) => {
+    setCartItems(removeCartItem(cartItems, productToRemove));
+  };
+
+  const clearItemFromCart = (productToClear) => {
+    setCartItems(clearCartItem(cartItems, productToClear));
   };
 
   useEffect(() => {
@@ -40,6 +72,14 @@ export const CartProvider = ({ children }) => {
     setCartCount(newCartItems);
   }, [cartItems]);
 
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (accumulator, item) => accumulator + item.price * item.quantity,
+      0
+    );
+    setTotal(newCartTotal);
+  }, [cartItems]);
+
   return (
     <CartContext.Provider
       value={{
@@ -48,6 +88,9 @@ export const CartProvider = ({ children }) => {
         cartItems,
         addItemToCart,
         cartCount,
+        removeItemFromCart,
+        clearItemFromCart,
+        total,
       }}
     >
       {children}
