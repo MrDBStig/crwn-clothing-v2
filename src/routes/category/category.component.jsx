16aa8c43dc7fa1c8a13ce1502d14b37ee755eae2
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-06-29 19:27:28 +0000

    Create Category page

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,30 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import { useProductContext } from "../../contexts/product.context";
+
+import ProductCard from "../../components/product-card/product-card.component";
+
+import "./category.styles.scss";
+
+const Category = () => {
+  const { category } = useParams();
+  const { products } = useProductContext();
+
+  const [categoryProducts, setCategoryProducts] = useState(products[category]);
+
+  useEffect(() => {
+    setCategoryProducts(products[category]);
+  }, [category, products]);
+
+  return (
+    <div className="category-container">
+      {categoryProducts &&
+        categoryProducts.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+    </div>
+  );
+};
+
+export default Category;
