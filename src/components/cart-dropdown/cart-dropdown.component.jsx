6371bca9ffa57714595dd7b278f1e810804385e7
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-06-29 14:57:05 +0000

    Create redirect to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
@@ -8,6 +10,10 @@ import "./cart-dropdown.styles.scss";
 const CartDropdown = () => {
   const { cartItems } = useCartContext();
 
+  const navigate = useNavigate();
+
+  const goToCheckout = () => navigate("/checkout");
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -15,7 +21,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} {...item} />
         ))}
       </div>
-      <Button>Go to checkout</Button>
+      <Button onClick={goToCheckout}>Go to checkout</Button>
     </div>
   );
 };
