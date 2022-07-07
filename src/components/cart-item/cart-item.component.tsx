718e9de2318596e8b25c754d800f2f4eceae7636
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-07-07 10:03:50 +0000

    Optimise our app

--- src/components/cart-item/cart-item.component.tsx
+++ src/components/cart-item/cart-item.component.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 import { CartItemContainer, ItemDetails } from "./cart-item.styles";
 
 import { CartItem as TCartItem } from "../../store/cart/cart.types";
@@ -7,7 +7,7 @@ type CartItemProps = {
   cartItem: TCartItem;
 };
 
-const CartItem: FC<CartItemProps> = ({ cartItem }) => {
+const CartItem: FC<CartItemProps> = memo(({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <CartItemContainer>
@@ -20,6 +20,6 @@ const CartItem: FC<CartItemProps> = ({ cartItem }) => {
       </ItemDetails>
     </CartItemContainer>
   );
-};
+});
 
 export default CartItem;
